4be0e124a9f8512ea88c966ba8f8ddda8c82f281
yjh <yjv1031@gmail.com>
2024-03-31 03:32:00 +0900

    이미지 리스트 조회 커밋

--- capsule-admin/src/pages/image/ImageMain.tsx
+++ capsule-admin/src/pages/image/ImageMain.tsx
@@ -3,10 +3,26 @@ import { commonStateStore } from "../../store/commonStore";
 import { DataGrid, GridColDef, GridEditBooleanCell } from "@mui/x-data-grid";
 import { Tooltip } from "@mui/material";
 import Checkbox from "@mui/material/Checkbox";
+import { produce } from "immer";
+
+interface ImageMasterType {
+    seq: number,
+    name: string,
+    useYn: string,
+    checked: boolean,
+    memberList: ImageMemberType[]
+};
+
+interface ImageMemberType {
+    seq: number,
+    order: number,
+    imgUrl: string
+}
 
 function ImageList() {
   const { setCurrentMenuKey, commonAjaxWrapper } = commonStateStore();
   const [searchKeyword, setSearchKeyword] = useState<string>('');
+  const [rows, setRows] = useState<ImageMasterType[]>([]);
 
   const searchKeywordChangehandler = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchKeyword(e.target.value);
@@ -16,15 +32,52 @@ function ImageList() {
       setCurrentMenuKey(2);
   },[]);
   
-  const columns: GridColDef<(typeof rows)[number]>[] = [
+  const columns: GridColDef<ImageMasterType>[] = [
     {
-        field: 'lastName',
+        field: 'checked',
+        headerName: '선택',
+        width: 70,
+        editable: false,
+        renderCell: (param) => {
+            
+            return (
+                <Checkbox checked={param.row.checked}
+                    onClick={() => {
+                        const newRows = produce(rows, (draft => {
+                            const row = draft.find(item => item.seq == param.row.seq);
+                            if(row) {
+                                row.checked = !row.checked;
+                            }
+                        }));
+                        setRows(newRows);
+                    }}
+                />
+            );
+        }
+    },
+    {
+        field: 'seq',
         headerName: '그룹 아이디',
+        width: 100,
+        editable: false,
+        sortable: false,
+        filterable: false,
+    },
+    {
+        field: 'name',
+        headerName: '그룹 명',
         width: 200,
         editable: false,
         sortable: false,
         filterable: false,
-        renderEditCell: GridEditBooleanCell
+    },
+    {
+        field: 'useYn',
+        headerName: '활성화 여부',
+        width: 100,
+        editable: false,
+        sortable: false,
+        filterable: false,
     },
     {
         field: 'fullName',
@@ -33,7 +86,7 @@ function ImageList() {
         sortable: false,
         filterable: false,
         width: 100,
-        valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+        valueGetter: (value, row) => `${row.memberList.length}`,
     },
     {
         field: 'imageUpdate',
@@ -59,9 +112,15 @@ function ImageList() {
             return (
                 <Tooltip title={
                     <div>
-                        <img src={params.value} alt="Preview" width="100" />
+                        {
+                            params.row.memberList.map(item => (
+                                <>
+                                    <img src={item.imgUrl} alt="Preview" width="100" />
+                                </>
+                            ))
+                        }
                     </div>
-                } arrow placement="top">
+                } arrow placement="right">
                     <button className="grid_image_thumnai_button">이미지 미리보기</button>
                 </Tooltip>
             );
@@ -69,23 +128,18 @@ function ImageList() {
     },
   ];
 
-  const [rows, setRows] = useState([
-    { id: '1herh', lastName: 'Snow', firstName: 'Jon', age: 14 },
-    { id: '2gfj', lastName: 'Lannister', firstName: 'Cersei', age: 31 },
-    { id: '3ncv', lastName: 'Lannister', firstName: 'Jaime', age: 31 },
-    { id: '4sd', lastName: 'Stark', firstName: 'Arya', age: 11 },
-    { id: '5hdf', lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: '6her', lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: '7jkty', lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: '8kkk', lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: '9fscgsdg', lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-  ]);
-
   const trySearchList = async() => {
-    const param = {
-
-    };
-    const data = await commonAjaxWrapper('get', '/image', param);
+    const param = {};
+    const data = await commonAjaxWrapper('get', `/image?page=${1}&size=${10}`, param);
+    if(data) {
+        const convertRows = data.content.map((item: ImageMasterType) => {
+            return {
+                ...item,
+                checked: false
+            }
+        });
+        setRows(convertRows);
+    }
   }
 
   useEffect(() => {
@@ -117,7 +171,7 @@ function ImageList() {
                     <DataGrid
                         rows={rows}
                         columns={columns}
-                        // getRowId={(row)=>(row.id)}
+                        getRowId={(row)=>(row.seq)}
                         // initialState={{
                         //     pagination: {
                         //         paginationModel: {
